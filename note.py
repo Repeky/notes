3a413582020f699a337396cd79e82b76f89a8cf7
Ivan <IvanKatauzov@gmail.com>
2023-12-30 06:16:00 +0300

    add func delete

--- note.py
+++ note.py
@@ -3,6 +3,7 @@ import autho
 
 NOTES_FILE = "notes.json"
 
+
 def load_notes():
     try:
         with open(NOTES_FILE, 'r') as file:
@@ -10,15 +11,18 @@ def load_notes():
     except FileNotFoundError:
         return {}
 
+
 def save_notes(notes):
     with open(NOTES_FILE, 'w') as file:
         json.dump(notes, file)
+
 
 def add_note(notes, username):
     note = input("Введите заметку: ")
     notes.setdefault(username, []).append(note)
     save_notes(notes)
     print("Заметка добавлена.")
+
 
 def look_notes(notes, username):
     user_notes = notes.get(username, [])
@@ -27,6 +31,26 @@ def look_notes(notes, username):
             print(f"{i}) {note}")
     else:
         print("У вас пока нет заметок.")
+
+
+def delete_note(notes, username):
+    user_notes = notes.get(username, [])
+    if not user_notes:
+        print("У вас пока нет заметок.")
+        return
+
+    look_notes(notes, username)
+    try:
+        note_index = int(input("Введите номер заметки для удаления: ")) - 1
+        if 0 <= note_index < len(user_notes):
+            del user_notes[note_index]
+            save_notes(notes)
+            print("Заметка удалена.")
+        else:
+            print("Неверный номер заметки.")
+    except ValueError:
+        print("Пожалуйста, введите число.")
+
 
 def main_menu():
     users = autho.load_users()
@@ -49,16 +73,20 @@ def main_menu():
                 print(f"\nПривет, {username}!\n"
                       "1) Добавить заметку\n"
                       "2) Посмотреть заметки\n"
-                      "3) Выход\n")
+                      "3) Удалить заметку\n"
+                      "4) Выход\n")
                 user_choice = input("Выберите действие: ")
                 if user_choice == "1":
                     add_note(notes, username)
                 elif user_choice == "2":
                     look_notes(notes, username)
                 elif user_choice == "3":
+                    delete_note(notes, username)
+                elif user_choice == "4":
                     break
                 else:
                     print("Введите корректно!\n")
+
 
 if __name__ == "__main__":
     main_menu()
